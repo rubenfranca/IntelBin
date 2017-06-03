0f02f224349ca7d339ce2108e6d3acab6238c1b7
Rúben França <ruben_tiagof@hotmail.com>
2017-06-03 13:05:32 +0100

    changes

--- resources/views/edificios.blade.php
+++ resources/views/edificios.blade.php
@@ -14,27 +14,27 @@
         <div class="col-md-8 col-md-offset-2">
             @foreach($piso_display as $pisos)
             <div class="panel panel-default">
-                <div class="panel-heading">{{$pisos->nome}}</div>
-                
-                 <div id="collapse1" class="panel-collapse collapse in">
-                    <div class="panel-body">
-                        @foreach($locais_display as $locais)
-                            @if($locais->piso_id == $pisos->id)
-                               <label for="local" class="col-md-4 control-label">Local: {{$locais->nome}}</label><br><hr>
-                                   @foreach($caixotes_display as $caixotes)
-                                       @if($caixotes->local_id == $locais->id)
-                                              <p><strong>Id: </strong>{{$caixotes->id}}</p>
-                                              <p><strong>Nome: </strong>{{$caixotes->nome}}</p>
-                                              <p><strong>Capacidade: </strong> {{$caixotes->capacidade}} L</p>
-                                              <p><strong>Nível atual: </strong> {{$caixotes->level}} %</p>
-                                              <p><strong>Tipo de Lixo: </strong> {{$caixotes->tipoLixo}} </p>
-                                           <hr>
-                                       @endif
-                                   @endforeach
-                            @endif
-                        @endforeach
+                    <div class="panel-heading">{{$pisos->nome}}</div>
+
+                     <div id="collapse1" class="panel-collapse collapse in">
+                        <div class="panel-body">
+                            @foreach($locais_display as $locais)
+                                @if($locais->piso_id == $pisos->id)
+                                   <label for="local" class="col-md-4 control-label">Local: {{$locais->nome}}</label><br><hr>
+                                       @foreach($caixotes_display as $caixotes)
+                                           @if($caixotes->local_id == $locais->id)
+                                                  <p><strong>Id: </strong>{{$caixotes->id}}</p>
+                                                  <p><strong>Nome: </strong>{{$caixotes->nome}}</p>
+                                                  <p><strong>Capacidade: </strong> {{$caixotes->capacidade}} L</p>
+                                                  <p><strong>Nível atual: </strong> {{$caixotes->level}} %</p>
+                                                  <p><strong>Tipo de Lixo: </strong> {{$caixotes->tipoLixo}} </p>
+                                               <hr>
+                                           @endif
+                                       @endforeach
+                                @endif
+                            @endforeach
+                        </div>
                     </div>
-                </div>
             
             
             </div>
