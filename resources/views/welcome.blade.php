de607eef41f569f782f1400277fc8bfcca70d839
Rúben França <ruben_tiagof@hotmail.com>
2017-06-03 15:21:42 +0100

    HomePage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,11 @@
 @extends('layout')
 @section('content')
+  <img src ="/images/Entrevista_Surreal_600.jpg" style="width:100%; height: 600px;">
    <div class="container">
+   
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
+        <h1>Bem-vindo ao IntelBin</h1>
         </div>
     </div>
     </div>
